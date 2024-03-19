75c65a49356d8c84012b63d63f4be18e9f21a7f4
Colganmiller <colgan.miller@ufl.edu>
2024-03-19 11:30:19 -0400

    made encode function and started main func

---
+++ main.py
@@ -0,0 +1,24 @@
+def encode(code):
+    string = ''
+    for i in code:
+        j = int(i)+3
+        if j >= 10:
+            j = str(j)
+            j = j[1]
+
+        string += str(j)
+
+    return string
+
+
+
+
+
+
+
+def main():
+    while True:
+        option = input("Choose an option: ")
+        print("1. encode")
+        print("2. decode")
+

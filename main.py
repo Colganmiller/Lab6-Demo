8d9a8f341e86e0b8177d4541df8f3f10fa4bc685
Colganmiller <colgan.miller@ufl.edu>
2024-03-20 10:34:42 -0400

    finished the menu options and if name == main

--- main.py
+++ main.py
@@ -17,11 +17,26 @@ def encode(code):
 
 
 
-
 def main():
     while True:
-        option = input("Choose an option: ")
-        print("1. encode")
-        print("2. decode")
+        print("Menu")
+        print("------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        option = int(input("Please enter an option: "))
 
+        if option == 1:
+            password = input("Please enter your password to encode:")
+            print("Your password has been encoded and stored!")
+            encode_num = encode(password)
+        elif option == 2:
+            encode_num = encode(password)
+            print(f"The encoded password is {encode_num}, and the original password is {password}.")
+        elif option == 3:
+            break
+
+if __name__ == "__main__":
+    main()
 
